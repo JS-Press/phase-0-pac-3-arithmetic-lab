02dd4622860e87be509436ccb2910a460d67ac07
Jarad Solomon <jaradsolomon@gmail.com>
2021-05-06 03:35:59 -0500

    Complete assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+let a = 5;
+let b = 4;
+let n = 3;
+let string = "2.222";
+
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide(a,b){
+    return a / b;
+}
+
+function increment(n){
+    ++n;
+    return n;
+}
+ function decrement(n){
+     n--;
+     return n;
+ }
+
+ 
+
+ function makeInt(n) {
+    return parseInt(n, 10);
+
+ }
+
+
+ function preserveDecimal(n) {
+    return parseFloat(n);
+    
+}
